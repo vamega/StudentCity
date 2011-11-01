54aa30860026bca46b1bc539ca656b25912fea42
Ken Bellows <grimertop90@gmail.com>
2011-11-01 17:34:41 -0400

    Added a TEMPLATE_CONTEXT_PROCESSORS item in settings.py, which should enable the {{ user }} object.

--- settings.py
+++ settings.py
@@ -100,6 +100,7 @@ TEMPLATE_LOADERS = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.auth',
     'django.core.context_processors.request',
 )
 
