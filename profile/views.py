25eac82f523bfd048f5c737c190d8ef53a5c7d64
Sean Lyons <slyons511@gmail.com>
2011-11-07 16:07:34 -0500

    fooling with a search alg, basic start, doesnt work

--- profile/views.py
+++ profile/views.py
@@ -27,3 +27,40 @@ def index(request):
     
 def settings(request):
     return render_to_response("profile/settings.html", {}, context_instance=RequestContext(request))
+    
+def search(request):
+    # csrf is used to prevent Cross-Site Request Forgeries (i.e. XSS attacks, SQL injections, etc.)
+    c = {}
+    c.update(csrf(request))
+    
+    if search.has_looked():
+        return HttpResponseRedirect("profile/course_search")
+    else:
+	found_classes = {}
+	found_dep = {}
+	found_number = {}
+	found_name = {}
+	
+        #c['search_form'] = SearchForm()
+        
+        for i in course_search.course_department:
+	    if i == models.Model.course_department:
+		temp = models.Model.course_department + ' ' + models.Model.course_number + ' ' + models.Model.course_name
+		found_dep.append(temp)
+		
+	for i in course_search.course_number:
+	    if i == models.Model.course_department:
+		temp = models.Model.course_department + ' ' + models.Model.course_number + ' ' + models.Model.course_name
+		found_number.append(temp)
+
+	for i in course_search.course_name:
+	    if i == models.Model.course_department:
+		temp = models.Model.course_department + ' ' + models.Model.course_number + ' ' + models.Model.course_name
+		found_name.append(temp)
+	    
+        
+        if request.GET.get('error') == '1':
+            c['error'] = 1
+        
+        
+        return render_to_response('profile/course_search.html', c, context_instance=RequestContext(request))
